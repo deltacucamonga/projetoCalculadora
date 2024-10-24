d1da518d4d755509024fbd2c09a63d7fa9cdc536
deltacucamonga <davicasamar@gmail.com>
2024-10-24 19:08:17 -0300

    calculadora

---
+++ App.js
@@ -0,0 +1,42 @@
+import { Text, View, StyleSheet, TouchableOpacity } from 'react-native';
+
+
+
+
+export default function App() {
+  return (
+    <View style={styles.container}> <View style={styles.butaoContainer}>
+     <TouchableOpacity styles={styles.butao}>
+      <Text styles={styles.butaoText}>
+        -
+      </Text>
+     </TouchableOpacity>
+    
+ 
+    </View>
+     </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  butaoText: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: 'Green'
+  },
+  butao: {
+    backgroundColor: 'black',
+    width: '20%',
+    borderRadius: 5,
+    alignItems: 'center'
+  }
+});
